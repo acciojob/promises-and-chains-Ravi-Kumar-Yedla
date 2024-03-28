1b160885b12d1e18b44f2bf39f23988103f18176
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-03-28 10:55:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
-// //your JS code here. If required.
-document.getElementById("myForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent form submission
-    const age = document.getElementById("age").value;
-    const name = document.getElementById("name").value;
 
-    if (age === "" || name === "") {
-        alert("Please fill out all fields");
-        return;
-    }
-
-    const promise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (parseInt(age) >= 18) {
-                resolve(`Welcome, ${name}. You can vote.`);
-            } else {
-                reject(`Oh sorry ${name}. You aren't old enough.`);
-            }
-        }, 4000);
-    });
-
-    promise.then((message) => {
-        alert(message);
-    }).catch((message) => {
-        alert(message);
-    });
+document.getElementById("myForm").addEventListener("submit",function(event){
+	event.preventDefault();
+	const name = document.getElementById("name").value;
+	const age = document.getElementById("age").value;
+	
+	if(name === "" || age === ""){
+		alert("please fill the form");
+		return;
+	}
+	const promise = new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(parseInt(age)>=18){
+				resolve(`Welcome, ${name}. You can vote.`);
+			}
+			else{
+				reject(`Oh sorry ${name}. You aren't old enough.`)
+			}
+		},4000);
+	})
+promise.then((message)=> alert(message));
+	promise.catch((message)=>alert(message));
+	
 });
 
 
@@ -31,39 +28,5 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
 
 
 
-// document.getElementById('myForm').addEventListener('submit',function(event){
-// 	event.preventDefault();
-// 	const name = document.getElementById("name").value;
-// 	const age = document.getElementById("age").value;
-	
-// 	if(name === "" || age === ""){
-// 		alert("please fill the form");
-// 		return;
-// 	}
-// 	const promise = new Promise((resolve,reject)=>{
-// 		setTimeOut(()=>{
-// 			if(parseInt(age)>=18){
-// 				resolve(`Welcome, ${name}. You can vote.`);
-// 			}
-// 			else{
-// 				reject(`Oh sorry ${name}. You aren't old enough.`)
-// 			}
-// 		},4000);
-// 	})
-// // promise.then((message)=> alert(message));
-// // 	promise.catch((message)=>alert(message));
-// 	 promise.then((message) => {
-//         alert(message);
-//     }).catch((message) => {
-//         alert(message);
-//     });
-
-
-
-
-
-
-
 
 	
-// }
