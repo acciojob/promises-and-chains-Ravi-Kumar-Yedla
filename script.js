4f0c001b0229243a33f31b055b7ab1bf1337cc0a
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-03-28 10:40:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+document.getElementById('myForm').addEventListener('submit',function(event){
+	event.preventDefault();
+	const name = document.getElementById("name").value;
+	const age = document.getElementById("age").value;
+	
+	if(name === "" || age === ""){
+		alert("please fill the form");
+		return;
+	}
+	const promise = new Promise((resolve,reject)=>{
+		setTimeOut(()=>{
+			if(parseInt(age)>=18){
+				resolve(`Welcome, ${name}. You can vote.`);
+			}
+			else{
+				reject(`Oh sorry ${name}. You aren't old enough.`)
+			}
+		},4000);
+	})
+// promise.then((message)=> alert(message));
+// 	promise.catch((message)=>alert(message));
+	 promise.then((message) => {
+        alert(message);
+    }).catch((message) => {
+        alert(message);
+    });
+
+
+
+
+
+
+
+
+	
+}
